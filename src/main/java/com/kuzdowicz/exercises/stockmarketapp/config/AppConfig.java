5197f7c051899a682aa074fb307e8e0aaf6de612
MartinKuzdowicz <martinkuzdowicz@gamil.com>
2016-07-25 22:27:56 +0200

    amendments to component scan configs

--- src/main/java/com/kuzdowicz/exercises/stockmarketapp/config/AppConfig.java
+++ src/main/java/com/kuzdowicz/exercises/stockmarketapp/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "com.kuzdowicz.stockmarketapp" })
+@ComponentScan(basePackages = { "com.kuzdowicz.exercises.stockmarketapp" })
 public class AppConfig {
 
 }
